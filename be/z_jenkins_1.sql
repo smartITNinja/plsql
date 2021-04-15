9960545cf798ef5be8e7ab715201aaf76d046d3a
root <root@web-front-linux-8.sub12111546370.reservhotel.oraclevcn.com>
2021-04-15 14:20:15 +0000

    commit any text

---
+++ be/z_jenkins_1.sql
@@ -0,0 +1,7 @@
+create or replace procedure z_jenkins_1
+is
+begin
+   -- version 2
+   htp.p('soy z_jenkins_1'); 
+end z_jenkins_1 ; 
+/
---
+++ extranet/agencies/z_jenkins_2.sql
@@ -0,0 +1,7 @@
+create or replace procedure z_jenkins_2
+is
+begin
+   -- version 1
+   htp.p('soy z_jenkins_2'); 
+end z_jenkins_2 ; 
+/
